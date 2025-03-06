ca7e4730a16bfe76c6df9fa4d07e9a0f81e82e86
ercan1s1k <113260584+ercan1s1k@users.noreply.github.com>
2025-03-06 05:26:45 +0300

    Create index.js
    js codes for header and page

---
+++ index.js
@@ -0,0 +1,145 @@
+function gri(){
+				var sayfa=document.getElementById('sf');
+				sayfa.style.backgroundColor="aqua";
+				var iletisim=document.getElementById('iletisim');
+				iletisim.style="background:aqua; color:black; animation: forTextOfMenu 4s linear infinite;" ;
+
+				
+				/// ekle ///
+				var anasayfa=document.getElementById('anasayfa');
+				anasayfa.style="background:black; color:blue; border:1px solid blue;";
+				var dis_basin=document.getElementById('dis_basin');
+				dis_basin.style="background:black; color:green; border:1px solid green;";
+				var son_dakika=document.getElementById('son_dakika');
+				son_dakika.style="background:black; color:red; border:1px solid red;";
+				var hakkimizda=document.getElementById('hakkimizda');
+				hakkimizda.style="background:black; color:yellow; border:1px solid yellow;";
+				var yerel_haberler=document.getElementById('yerel_haberler');
+				yerel_haberler.style="background:black; color:orange; border:1px solid orange;";
+				var siteye_giris=document.getElementById('siteye_giris');
+				siteye_giris.style="background:black; color:white; border:1px solid white;";
+			}
+
+			function kirmizi(){
+				var sayfa=document.getElementById('sf');
+				sayfa.style.backgroundColor="red";
+				var son_dakika=document.getElementById('son_dakika');
+				son_dakika.style="background:red; color:black;  animation: forTextOfMenu 4s linear infinite;";
+				
+				// Bu fonksiyonunda bundan sonraki kod satÄ±rlarÄ± diÄŸer menÃ¼leri (tekrar)dÃ¼zenler.
+				var anasayfa=document.getElementById('anasayfa');
+				anasayfa.style="background:black; color:blue; border:1px solid blue;";
+				var yerel_haberler=document.getElementById('yerel_haberler');
+				yerel_haberler.style="background:black; color:orange; border:1px solid orange;";
+				var siteye_giris=document.getElementById('siteye_giris');
+				siteye_giris.style="background:black; color:white; border:1px solid white;";
+				var iletisim=document.getElementById('iletisim');
+				iletisim.style="background:black; color:aqua; border:1px solid aqua;";
+				var dis_basin=document.getElementById('dis_basin');
+				dis_basin.style="background:black; color:green; border:1px solid green;";
+				var hakkimizda=document.getElementById('hakkimizda');
+				hakkimizda.style="background:black; color:yellow; border:1px solid yellow;";
+			}
+
+			function sari(){
+				var sayfa=document.getElementById('sf');
+				var hakkimizda=document.getElementById('hakkimizda');
+				sayfa.style.backgroundColor="yellow";
+				hakkimizda.style="background:yellow; color:black;  animation: forTextOfMenu 4s linear infinite;";
+				
+				// Bu fonksiyonunda bundan sonraki kod satÄ±rlarÄ± diÄŸer menÃ¼leri (tekrar)dÃ¼zenler.
+				var anasayfa=document.getElementById('anasayfa');
+				anasayfa.style="background:black; color:blue; border:1px solid blue;";
+				var son_dakika=document.getElementById('son_dakika');
+				son_dakika.style="background:black; color:red; border:1px solid red;";
+				var yerel_haberler=document.getElementById('yerel_haberler');
+				yerel_haberler.style="background:black; color:orange; border:1px solid orange;";
+				var siteye_giris=document.getElementById('siteye_giris');
+				siteye_giris.style="background:black; color:white; border:1px solid white;";
+				var iletisim=document.getElementById('iletisim');
+				iletisim.style="background:black; color:aqua; border:1px solid aqua;";
+				var dis_basin=document.getElementById('dis_basin');
+				dis_basin.style="background:black; color:green; border:1px solid green;";
+			}
+
+			function yesil(){
+				var sayfa=document.getElementById('sf');
+				sayfa.style.backgroundColor="green";
+				var dis_basin=document.getElementById('dis_basin');
+				dis_basin.style="background:green; color:black;  animation: forTextOfMenu 4s linear infinite;";
+				
+				// Bu fonksiyonunda bundan sonraki kod satÄ±rlarÄ± diÄŸer menÃ¼leri (tekrar)dÃ¼zenler.
+				var anasayfa=document.getElementById('anasayfa');
+				anasayfa.style="background:black; color:blue; border:1px solid blue;";
+				var son_dakika=document.getElementById('son_dakika');
+				son_dakika.style="background:black; color:red; border:1px solid red;";
+				var yerel_haberler=document.getElementById('yerel_haberler');
+				yerel_haberler.style="background:black; color:orange; border:1px solid orange;";
+				var hakkimizda=document.getElementById('hakkimizda');
+				hakkimizda.style="background:black; color:yellow; border:1px solid yellow;";
+				var siteye_giris=document.getElementById('siteye_giris');
+				siteye_giris.style="background:black; color:white; border:1px solid white;";
+				var iletisim=document.getElementById('iletisim');
+				iletisim.style="background:black; color:aqua; border:1px solid aqua;";
+			}
+
+			function turuncu(){
+				var sayfa=document.getElementById('sf');
+				sayfa.style="background:orange; color:white;";
+				var yerel_haberler=document.getElementById('yerel_haberler');
+				yerel_haberler.style="background:orange; color:black;  animation: forTextOfMenu 4s linear infinite;";
+				
+				// Bu fonksiyonunda bundan sonraki kod satÄ±rlarÄ± diÄŸer menÃ¼leri (tekrar)dÃ¼zenler.
+				var anasayfa=document.getElementById('anasayfa');
+				anasayfa.style="background:black; color:blue; border:1px solid blue;";
+				var son_dakika=document.getElementById('son_dakika');
+				son_dakika.style="background:black; color:red; border:1px solid red;";
+				var hakkimizda=document.getElementById('hakkimizda');
+				hakkimizda.style="background:black; color:yellow; border:1px solid yellow;";
+				var siteye_giris=document.getElementById('siteye_giris');
+				siteye_giris.style="background:black; color:white; border:1px solid white;";
+				var iletisim=document.getElementById('iletisim');
+				iletisim.style="background:black; color:aqua; border:1px solid aqua;";
+				var dis_basin=document.getElementById('dis_basin');
+				dis_basin.style="background:black; color:green; border:1px solid green;";
+			}
+
+			function mavi(){
+				var sayfa=document.getElementById('sf');
+				sayfa.style.backgroundColor="blue";
+				var anasayfa=document.getElementById('anasayfa');	
+				anasayfa.style="background:blue; color:black;  animation: forTextOfMenu 4s linear infinite;";
+				
+				// Bu fonksiyonunda bundan sonraki kod satÄ±rlarÄ± diÄŸer menÃ¼leri (tekrar)dÃ¼zenler.
+				var dis_basin=document.getElementById('dis_basin');
+				dis_basin.style="background:black; color:green; border:1px solid green;";
+				var son_dakika=document.getElementById('son_dakika');
+				son_dakika.style="background:black; color:red; border:1px solid red;";
+				var hakkimizda=document.getElementById('hakkimizda');
+				hakkimizda.style="background:black; color:yellow; border:1px solid yellow;";
+				var siteye_giris=document.getElementById('siteye_giris');
+				siteye_giris.style="background:black; color:white; border:1px solid white;";
+				var iletisim=document.getElementById('iletisim');
+				iletisim.style="background:black; color:aqua; border:1px solid aqua;";
+				var yerel_haberler=document.getElementById('yerel_haberler');
+				yerel_haberler.style="background:black; color:orange; border:1px solid orange;";
+			}
+			
+			function beyaz(){
+				var sayfa=document.getElementById('sf');
+				sayfa.style.backgroundColor="white";
+				var siteye_giris=document.getElementById('siteye_giris');
+				siteye_giris.style="background:white; color:black;  animation: forTextOfMenu 4s linear infinite;";
+				
+				// Bu fonksiyonunda bundan sonraki kod satÄ±rlarÄ± diÄŸer menÃ¼leri (tekrar)dÃ¼zenler.
+				var son_dakika=document.getElementById('son_dakika');
+				son_dakika.style="background:black; color:red; border:1px solid red;";
+				var dis_basin=document.getElementById('dis_basin');
+				dis_basin.style="background:black; color:green; border:1px solid green;";
+				var hakkimizda=document.getElementById('hakkimizda');
+				hakkimizda.style="background:black; color:yellow; border:1px solid yellow;";
+				var iletisim=document.getElementById('iletisim');
+				iletisim.style="background:black; color:aqua; border:1px solid aqua;";
+				var yerel_haberler=document.getElementById('yerel_haberler');
+				yerel_haberler.style="background:black; color:orange; border:1px solid orange;";
+			}
